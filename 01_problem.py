d4099263409825b03b05f3d64660461cca7d90de
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-11 21:20:53 +0530

    modules question

---
+++ 01_problem.py
@@ -0,0 +1,9 @@
+'''Imports the math module
+Takes a number from the user
+Prints the square root and factorial of that number'''
+import math
+user=int(input("Enter number:"))
+sqrt=math.sqrt(user)
+print("Square root",sqrt)
+fact=math.factorial(user)
+print("Factorial:",fact)
