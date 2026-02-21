0ddc25ee448fbfb50517857aafad9db895c16dd3
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-21 21:03:37 +0530

    sqrt(1-10)

---
+++ 04_problem.py
@@ -0,0 +1,7 @@
+'''Write a program that:
+Imports only the sqrt function from the math module
+Calculates square root of numbers from 1 to 10'''
+import math
+for i in range(1,11):
+    square=math.sqrt(i)
+    print(square)
