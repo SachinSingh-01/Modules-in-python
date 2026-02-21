82543c84857986f1a23059ac2946f493985da3a0
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-21 20:52:54 +0530

    datetime

---
+++ 03_problem.py
@@ -0,0 +1,7 @@
+'''Import with Alias
+Write a program that:
+Imports the datetime module using an alias
+Prints the current date and time'''
+import datetime as dt
+now=dt.datetime.now()
+print(now)
