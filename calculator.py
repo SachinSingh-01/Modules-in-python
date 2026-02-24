1ac8a48da318b5e29ab7313e44456b36c779eca1
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-24 20:48:57 +0530

    module calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+'''Create Your Own Module (Basic)
+Step by step:
+Create a file named calculator.py
+Write functions:
+add(a, b)
+subtract(a, b)
+Create another file main.py
+Import the module and call both functions'''
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+    
---
+++ call.py
@@ -0,0 +1,3 @@
+import calculator
+print("Addition:",calculator.add(10,2))
+print("Subtraction:",calculator.sub(10,2))
