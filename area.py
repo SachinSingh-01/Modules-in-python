65a5917b30a2f35795fc0be326cb4dddd194f2d3
Sachin Singh <sachinsingh19012@gmail.com>
2026-02-24 20:59:01 +0530

    find_area_circle_rectangle

---
+++ area.py
@@ -0,0 +1,8 @@
+'''Create a module area.py that contains:
+area_circle(r)
+area_rectangle(l, w)
+Then import it and calculate areas.'''
+def area_circle(r):
+    return 3.14*r*r
+def area_rectangle(l,w):
+    return l*w
---
+++ call_area.py
@@ -0,0 +1,3 @@
+import area
+print("Area of the circle:",area.area_circle(1))
+print("Area of the rectangle:",area.area_rectangle(1,1))
